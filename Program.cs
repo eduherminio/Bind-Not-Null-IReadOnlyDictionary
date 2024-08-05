a166c17b6911fe8aee572f0dbce833fa8356f66a
Eduardo Caceres <edcaceres@microsoft.com>
2024-08-05 18:36:53 +0200

    Add repro

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+MyOptions instance = new();
+var section = config.GetSection("Options");
+section.Bind(instance/*, opt => opt.ErrorOnUnknownConfiguration = true*/);
+
+Console.WriteLine(instance.Enabled);
+Console.WriteLine(instance.Fields.Count);
+
+public class MyOptions
+{
+    public bool Enabled { get; set; } = false;
+
+    public IReadOnlyDictionary<string, object> Fields { get; set; } = new Dictionary<string, object>();
+
+    // Workaround:
+
+    //#if NET8_0_OR_GREATER
+    //    = new Dictionary<string, object>();
+    //#else
+    //    //= null!;
+    //#endif
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# IReadOnlyDictionaryIssue
+
+Repro:
+
+```csharp
+dotnet run --framework net8.0
+dotnet run --framework net6.0
+```
